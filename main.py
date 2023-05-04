75d29a97cb02dc76c8634e5eaa5743b93cc33a8f
Stephan Lemmer <lemmersj@umich.edu>
2023-05-04 14:33:07 -0400

    Running black and pylint.

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 """A simple script for training and evaluating BLOOM on the AI2-ARC dataset."""
-from datasets import load_dataset
 import argparse
-from torch.optim import AdamW, lr_scheduler
-from torch.utils.data import DataLoader, Dataset
-from peft import PromptTuningConfig, TaskType, PromptTuningInit, get_peft_model, LoraConfig, PrefixTuningConfig, PeftModel
-import wandb
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import numpy as np
-import pdb
 import random
 import time
 import os
 import sys
 
+from torch.optim import AdamW, lr_scheduler
+from torch.utils.data import DataLoader, Dataset
+from peft import (
+    PromptTuningConfig,
+    TaskType,
+    PromptTuningInit,
+    get_peft_model,
+    LoraConfig,
+    PrefixTuningConfig,
+    PeftModel,
+)
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from datasets import load_dataset
+
+import numpy as np
+import wandb
+
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-class BloomWrapper():
+
+class BloomWrapper:
     """An object containing convenience functions for bloom."""
+
     def __init__(self, args):
         """Initializes the object, sets self variables, etc.
 
@@ -32,63 +43,68 @@ class BloomWrapper():
         # Set the finetune method.
         if args.finetune_method == "prompt":
             peft_config = PromptTuningConfig(
-                task_type = TaskType.CAUSAL_LM,
+                task_type=TaskType.CAUSAL_LM,
                 prompt_tuning_init=PromptTuningInit.TEXT,
                 num_virtual_tokens=args.virtual_tokens,
-                prompt_tuning_init_text=\
-                "Please answer the following multiple choice question:\n",
-                tokenizer_name_or_path=args.base_model)
+                prompt_tuning_init_text="Please answer the following multiple choice question:\n",
+                tokenizer_name_or_path=args.base_model,
+            )
         elif args.finetune_method == "lora":
             peft_config = LoraConfig(
                 task_type=TaskType.CAUSAL_LM,
                 inference_mode=False,
                 r=args.lora_r,
                 lora_alpha=args.lora_alpha,
-                lora_dropout=args.lora_dropout)
+                lora_dropout=args.lora_dropout,
+            )
         elif args.finetune_method == "prefix":
             peft_config = PrefixTuningConfig(
-                task_type=TaskType.CAUSAL_LM,
-                num_virtual_tokens=30)
+                task_type=TaskType.CAUSAL_LM, num_virtual_tokens=30
+            )
         else:
             print("Invalid finetune method specified.")
             sys.exit()
-        
+
         # Load the model and tokenizer
         self.model = AutoModelForCausalLM.from_pretrained(args.base_model)
         self.tokenizer = AutoTokenizer.from_pretrained(args.base_model)
-        self.tokenizer.model_max_length = 256 # This was set in the example
+        self.tokenizer.model_max_length = 256  # This was set in the example
 
         # Initialize or load the finetuned parameters.
         if args.finetuned_model is not None:
-            self.model = PeftModel.from_pretrained(self.model, args.finetuned_model) 
+            self.model = PeftModel.from_pretrained(self.model, args.finetuned_model)
         else:
             self.model = get_peft_model(self.model, peft_config)
 
         self.model.to(self.device)
-        
+
         # Set tracking variables.
-        self.losses = [] # Training loss
-        self.val_correct = [] # Is the answer correct?
-        self.answer_valid = [] # Is the answer valid (i.e., in the option set)
+        self.losses = []  # Training loss
+        self.val_correct = []  # Is the answer correct?
+        self.answer_valid = []  # Is the answer valid (i.e., in the option set)
 
     def training_step(self, batch):
         """Peforms a training step.
 
         args:
             batch: a batch of data.
-        
+
         Returns: loss
         """
         # Tokenize the input.
         tokenized_batch = self.tokenizer(
-            batch['full_phrase'], return_tensors="pt", padding="max_length",
-            truncation=True)
+            batch["full_phrase"],
+            return_tensors="pt",
+            padding="max_length",
+            truncation=True,
+        )
 
         # Re-map the information from the tokenizer.
-        new_batch = {"input_ids": tokenized_batch['input_ids'].to(self.device),
-                     "labels": tokenized_batch['input_ids'].to(self.device),
-                     "attention_mask": tokenized_batch['attention_mask'].to(
-                         self.device)}
+        new_batch = {
+            "input_ids": tokenized_batch["input_ids"].to(self.device),
+            "labels": tokenized_batch["input_ids"].to(self.device),
+            "attention_mask": tokenized_batch["attention_mask"].to(self.device),
+        }
 
         # Perform the forward pass
         result = self.model(**new_batch)
@@ -117,24 +133,27 @@ class BloomWrapper():
         """
         # Tokenize the input batch.
         tokenized_batch = self.tokenizer(
-            batch['prompt'], return_tensors="pt", padding="max_length",
-            truncation=True)
+            batch["prompt"], return_tensors="pt", padding="max_length", truncation=True
+        )
 
         # Tokenize the prompt. The maximum number of generated tokens is the
         # size of the longest tokenization minus the size of the prompt.
         tokenized_for_length = self.tokenizer(
-            batch['full_phrase'], return_tensors="pt", padding="max_length",
-            truncation=True)['attention_mask'].sum(axis=1)
-        num_in_prompt = tokenized_batch['attention_mask'].sum(axis=1)
+            batch["full_phrase"],
+            return_tensors="pt",
+            padding="max_length",
+            truncation=True,
+        )["attention_mask"].sum(axis=1)
+        num_in_prompt = tokenized_batch["attention_mask"].sum(axis=1)
         max_new_tokens = (tokenized_for_length - num_in_prompt).max()
 
         # Generate the answer. Added some to max_new_tokens due to observed
         # errors. TODO: Figure out why this is happening.
         result = self.model.generate(
-            input_ids = tokenized_batch['input_ids'].to(self.device),
-            attention_mask=tokenized_batch['attention_mask'].to(self.device),
-            max_new_tokens=max_new_tokens+5)
-
+            input_ids=tokenized_batch["input_ids"].to(self.device),
+            attention_mask=tokenized_batch["attention_mask"].to(self.device),
+            max_new_tokens=max_new_tokens + 5,
+        )
 
         # Evaluate the answers one-by-one
         # TODO: Consider moving the string cleaning to a separate fn.
@@ -145,23 +164,24 @@ class BloomWrapper():
             answer = answer.replace("</s>", "")
             answer = answer.replace("<pad>", "")
             answer = answer.strip()
-           
-            if args.print_eval:
-                print(batch['prompt'][i])
+
+            if print_eval:
+                print(batch["prompt"][i])
                 print(f"Target: {batch['target'][i]}, Guess: {answer}")
                 print("---")
-                pdb.set_trace()
 
             # Is the answer correct?
             self.val_correct.append(
-                batch['target'][i].lower().strip().replace(
-                    ".", "") == answer.lower().strip().replace(".", ""))
+                batch["target"][i].lower().strip().replace(".", "")
+                == answer.lower().strip().replace(".", "")
+            )
 
             # Is the answer valid (i.e., does it match a given option?)
             self.answer_valid.append(
-                answer.lower().strip().replace(
-                    ".", "") in batch['prompt'][i].lower().strip().replace(
-                        ".", ""))
+                answer.lower().strip().replace(".", "")
+                in batch["prompt"][i].lower().strip().replace(".", "")
+            )
+
     def on_validation_epoch_end(self):
         """Finishes a validation epoch. Logs and resets variables.
 
@@ -173,8 +193,9 @@ class BloomWrapper():
 
         where_valid = np.where(np.array(self.answer_valid))[0]
         if where_valid.shape[0] > 0:
-            out_dict["val/correct_given_valid"] = np.array(
-                self.val_correct)[where_valid].mean()
+            out_dict["val/correct_given_valid"] = np.array(self.val_correct)[
+                where_valid
+            ].mean()
 
         wandb.log(out_dict)
         self.answer_valid = []
@@ -192,15 +213,20 @@ class BloomWrapper():
         # TODO: translate parameters to command line args.
         optimizer = AdamW(self.model.parameters(), lr=self.start_lr)
         scheduler = lr_scheduler.LinearLR(
-            optimizer, start_factor=1, end_factor=1/self.end_lr_divisor,
-            total_iters=120)
+            optimizer,
+            start_factor=1,
+            end_factor=1 / self.end_lr_divisor,
+            total_iters=120,
+        )
         return optimizer, scheduler
+
 
 class MCQADataset(Dataset):
     """A class meant to simplify multiple choice problems
-    
+
     Note: this is likely not sufficiently general to extend beyond AI2-ARC
     """
+
     def __init__(self, base_dataset, split, n_shot):
         """Initializes the dataset.
 
@@ -208,15 +234,15 @@ class MCQADataset(Dataset):
             base_dataset: a multiple-choice dataset from huggingface hub.
             split: train/val/test
             n_shot: how many examples to give the model as a prefix.
-        
+
         returns: nothing.
         """
         self.n_shot = n_shot
 
         # Initialize containers for the data.
-        self.prompt_strings = [] # The questions that are given to the model.
-        self.target_strings = [] # The target outputs.
-        self.dataset_strings = [] # The prompt and the target together.
+        self.prompt_strings = []  # The questions that are given to the model.
+        self.target_strings = []  # The target outputs.
+        self.dataset_strings = []  # The prompt and the target together.
 
         for item in base_dataset[split]:
             prompt, target, dataset = self.build_question(item)
@@ -224,8 +250,8 @@ class MCQADataset(Dataset):
             self.target_strings.append(target)
             self.dataset_strings.append(dataset)
 
-        self.train_strings = [] # Training strings for n-shot learning.
-        for item in base_dataset["train"]: # Prompts are only from train dataset.
+        self.train_strings = []  # Training strings for n-shot learning.
+        for item in base_dataset["train"]:  # Prompts are only from train dataset.
             _, _, dataset = self.build_question(item)
             self.train_strings.append(dataset)
 
@@ -253,9 +279,11 @@ class MCQADataset(Dataset):
 
         prompt_string += self.prompt_strings[i]
 
-        to_return = {"prompt": prompt_string,
-                     "full_phrase": prompt_string + self.target_strings[i],
-                     "target": self.target_strings[i]}
+        to_return = {
+            "prompt": prompt_string,
+            "full_phrase": prompt_string + self.target_strings[i],
+            "target": self.target_strings[i],
+        }
 
         return to_return
 
@@ -269,35 +297,42 @@ class MCQADataset(Dataset):
         concatentated.
         """
         base_prompt = ""
-        base_prompt += item['question']
+        base_prompt += item["question"]
         base_prompt += "\n"
 
-        for j in range(len(item['choices']['label'])):
-            base_prompt += item['choices']['label'][j]
+        for j in range(len(item["choices"]["label"])):
+            base_prompt += item["choices"]["label"][j]
             base_prompt += ". "
-            base_prompt += item['choices']['text'][j]
+            base_prompt += item["choices"]["text"][j]
             base_prompt += "\n"
-     
+
         base_prompt += "\nAnswer: "
 
-        correct_answer = item['answerKey']
+        correct_answer = item["answerKey"]
         correct_answer_idx = np.where(
-            np.array(item['choices']['label'])==correct_answer)[0]
+            np.array(item["choices"]["label"]) == correct_answer
+        )[0]
         assert len(correct_answer_idx) == 1
-        
-        target = f"{item['answerKey']}. {item['choices']['text'][correct_answer_idx.item()]}"
 
-        return (base_prompt, target, base_prompt+target)
+        target = (
+            f"{item['answerKey']}. {item['choices']['text'][correct_answer_idx.item()]}"
+        )
+
+        return (base_prompt, target, base_prompt + target)
+
 
 if __name__ == "__main__":
-    
     # Get and set command line args.
     # TODO: add help.
     parser = argparse.ArgumentParser()
     parser.add_argument("--start_lr", type=float)
     parser.add_argument("--base_model", type=str, required=True)
-    parser.add_argument("--finetune_method", type=str,
-                        choices=["prompt","prefix","lora"], required=True)
+    parser.add_argument(
+        "--finetune_method",
+        type=str,
+        choices=["prompt", "prefix", "lora"],
+        required=True,
+    )
     parser.add_argument("--virtual_tokens", type=int)
     parser.add_argument("--lora_r", type=int)
     parser.add_argument("--lora_alpha", type=float)
@@ -309,52 +344,66 @@ if __name__ == "__main__":
     parser.add_argument("--batch_size", type=int, default=2)
     parser.add_argument("--eval_only", action="store_true", default=False)
     parser.add_argument("--print_eval", action="store_true", default=False)
-    parser.add_argument("--split", type=str, required=True,
-                        choices=["ARC-Easy", "ARC-Challenge"])
+    parser.add_argument(
+        "--split", type=str, required=True, choices=["ARC-Easy", "ARC-Challenge"]
+    )
     args = parser.parse_args()
 
     model = BloomWrapper(args)
     base_dataset = load_dataset("ai2_arc", args.split)
     val_dataset = MCQADataset(base_dataset, "validation", n_shot=args.n_shot)
-    val_loader = DataLoader(val_dataset, batch_size=args.batch_size,
-                            shuffle=False, num_workers=os.cpu_count())
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=args.batch_size,
+        shuffle=False,
+        num_workers=os.cpu_count(),
+    )
     train_dataset = MCQADataset(base_dataset, "train", n_shot=args.n_shot)
-    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=os.cpu_count())
-   
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=args.batch_size,
+        shuffle=True,
+        num_workers=os.cpu_count(),
+    )
+
     if not args.eval_only:
         wandb.init(project="tune-bloom", config=args)
         optimizer, scheduler = model.configure_optimizers()
         best_acc = 0
         best_valid = 0
         best_acc_given_valid = 0
-        
-        out_dir = f"saved_models/{args.base_model.split('/')[-1]}-{args.finetune_method}-{args.virtual_tokens}-{args.n_shot}-{args.split}-{str(time.time()).split('.')[0]}/"
+
+        out_dir = (
+            f"saved_models/{args.base_model.split('/')[-1]}-"
+            "{args.finetune_method}-{args.virtual_tokens}-{args.n_shot}-"
+            "{args.split}-{str(time.time()).split('.', maxsplit=1)[0]}/"
+        )
     for epoch in range(120):
         model.model.eval()
         i = 0
         for eval_data in val_loader:
             model.validation_step(eval_data, args.print_eval)
-            i+=1
+            i += 1
         to_log = model.on_validation_epoch_end()
-        
+
         # If we're only doing evaluation, print the results and exit.
         if args.eval_only:
             print(to_log)
             sys.exit()
         if to_log["val/acc"] > best_acc:
             best_acc = to_log["val/acc"]
-            model.model.save_pretrained(out_dir+"best_acc")
+            model.model.save_pretrained(out_dir + "best_acc")
             wandb.log({"val/best_acc": best_acc})
         if to_log["val/valid"] > best_valid:
             best_valid = to_log["val/valid"]
-            model.model.save_pretrained(out_dir+"best_valid")
+            model.model.save_pretrained(out_dir + "best_valid")
             wandb.log({"val/best_valid": best_valid})
         if to_log["val/correct_given_valid"] > best_acc_given_valid:
             best_acc_given_valid = to_log["val/correct_given_valid"]
-            model.model.save_pretrained(out_dir+"best_given_valid")
+            model.model.save_pretrained(out_dir + "best_given_valid")
             wandb.log({"val/best_acc_given_valid": best_acc_given_valid})
 
-        model.model.save_pretrained(out_dir+"last")
+        model.model.save_pretrained(out_dir + "last")
 
         model.model.train()
 
@@ -364,7 +413,7 @@ if __name__ == "__main__":
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
-            i+=1
+            i += 1
 
         model.training_epoch_end()
         wandb.log({"train/epoch": epoch, "train/lr": scheduler.get_last_lr()[0]})
